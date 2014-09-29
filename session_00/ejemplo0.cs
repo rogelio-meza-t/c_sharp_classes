4cc54aa0408a63b87a5fdac11d72c380fed81171
Rogelio Meza <rogelio.meza.t@gmail.com>
2014-09-29 00:11:12 -0300

    add examples to session 0

---
+++ session_00/ejemplo0.cs
@@ -0,0 +1,12 @@
+using System;
+namespace EjemploHolaMundo
+{
+   class HolaMundo
+   {
+      static void Main(string[] args)
+      {
+         /* este es un comentario */
+         Console.WriteLine("Hola Mundo");
+      }
+   }
+}
